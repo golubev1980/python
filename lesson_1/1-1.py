9efe9f030b0fcfd3c225cb491e6c1a4081fd37ef
golubev1980 <77721142+golubev1980@users.noreply.github.com>
2021-01-25 17:30:10 +0300

    tasks_1.1-1.6

---
+++ lesson_1/1-1.py
@@ -0,0 +1,10 @@
+name = input('Как вас зовут? ')
+print('Привет, ', name, '!')
+
+age = int(input('Сколько вам лет? '))
+period = 10
+age_period = age + period
+year = 2021
+past_year = year - age
+print('Через', period, 'лет ваш возраст будет', age_period, 'лет')
+print('А ровно', age, 'лет назад на дворе был', past_year, 'год')
