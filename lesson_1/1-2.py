ce1bd1857794095640b4200ea83137d2d1568733
golubev1980 <77721142+golubev1980@users.noreply.github.com>
2021-01-25 17:20:19 +0300

    Delete lesson_1 directory

--- lesson_1/1-2.py
+++
@@ -1,8 +0,0 @@
-input_seconds = int(input('Введите любое число секунд: '))
-print('Вы ввели значение', input_seconds, 'секунд')
-
-hours = input_seconds // 3600
-minutes = (input_seconds - hours * 3600) // 60
-seconds = input_seconds % 60
-
-print(f'В формате чч:мм:сс это будет равно: {hours}:{minutes}:{seconds}')
--- lesson_1/1-3.py
+++
@@ -1,7 +0,0 @@
-n = input('Введите любое число n: ')
-nn = n + n
-nnn = n + n + n
-
-result = int(n) + int(nn) + int(nnn)
-
-print(f'Считаем: {n} + {nn} + {nnn} = {result}')
